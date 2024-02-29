e2f112aad5d7443304de3483b5ef9245f1cd57a7
riccardo.belluzzo <riccardo.belluzzo@allegro.com>
2024-02-29 16:01:24 +0100

    model names moved to enumerables.py

--- allms/models/__init__.py
+++ allms/models/__init__.py
@@ -1,5 +1,6 @@
 from typing import Type
 
+from allms.domain.enumerables import AvailableModels
 from allms.models.abstract import AbstractModel
 from allms.models.azure_llama2 import AzureLlama2Model
 from allms.models.azure_mistral import AzureMistralModel
@@ -12,10 +13,10 @@ __all__ = ["AzureOpenAIModel", "AzureLlama2Model", "AzureMistralModel", "VertexA
 
 def get_available_models() -> dict[str, Type[AbstractModel]]:
     return {
-        "azure_openai": AzureOpenAIModel,
-        "azure_llama2": AzureLlama2Model,
-        "azure_mistral": AzureMistralModel,
-        "vertexai_palm2": VertexAIPalmModel,
-        "vertexai_gemini": VertexAIGeminiModel
+        AvailableModels.AZURE_OPENAI_MODEL: AzureOpenAIModel,
+        AvailableModels.AZURE_LLAMA2_MODEL: AzureLlama2Model,
+        AvailableModels.AZURE_MISTRAL_MODEL: AzureMistralModel,
+        AvailableModels.VERTEXAI_PALM2_MODEL: VertexAIPalmModel,
+        AvailableModels.VERTEXAI_GEMINI_MODEL: VertexAIGeminiModel
     }
 
