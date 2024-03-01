57ed3673c987ce98c02c98cbc5142219e2d98212
Piotr C Zielinski <piotr.c.zielinski@allegro.pl>
2024-03-01 17:37:09 +0100

    Add Gemma to __init__.py

--- allms/models/__init__.py
+++ allms/models/__init__.py
@@ -7,6 +7,7 @@ from allms.models.azure_mistral import AzureMistralModel
 from allms.models.azure_openai import AzureOpenAIModel
 from allms.models.vertexai_gemini import VertexAIGeminiModel
 from allms.models.vertexai_palm import VertexAIPalmModel
+from allms.models.vertexai_gemma import VertexAIGemmaModel
 
 __all__ = [
     "AzureOpenAIModel",
@@ -14,6 +15,7 @@ __all__ = [
     "AzureMistralModel",
     "VertexAIPalmModel",
     "VertexAIGeminiModel",
+    "VertexAIGemmaModel",
     "get_available_models"
 ]
 
