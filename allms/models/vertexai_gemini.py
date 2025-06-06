be17b5b247a30790f5c11faeaaff2a1ec38ef030
Igor Marczak <igor.marczak@allegro.com>
2025-06-06 18:40:54 +0200

    introduce credentials in  and delete extra_headers

--- allms/models/vertexai_gemini.py
+++ allms/models/vertexai_gemini.py
@@ -70,6 +70,7 @@ class VertexAIGeminiModel(AbstractModel):
             location=self._config.cloud_location,
             api_endpoint=self._config.api_endpoint,
             api_transport=self._config.api_transport,
+            credentials=self._config.credentials,
         )
         # NOTE: this param is for some reason not passed, see: langchain_google_vertexai.llms.VertexAI.validate_environment
         # `endpoint_version` is not passed to the `ChatVertexAI` constructor
@@ -77,13 +78,6 @@ class VertexAIGeminiModel(AbstractModel):
         # it's set v1beta1 by default
         if self._config.endpoint_version:
             llm.client.endpoint_version = self._config.endpoint_version
-        # NOTE: `ChatVertexAI` is child of _VertexAICommon and grandchild of _VertexAIBase, the same as `VertexAI`,
-        # so they use the same validation in langchain_google_vertexai._base._VertexAIBase.validate_params_base.
-        # In `validate_params_base` the `default_metadata` is set to `additional_headers`.
-        # And in constructor of `ChatVertexAI` `additional_headers` is not passed.
-        # So `default_metadata` is always set to default value.
-        if self._config.extra_headers:
-            llm.client.default_metadata = self._config.extra_headers
         return llm
 
     def _get_prompt_tokens_number(self, prompt: ChatPromptTemplate, input_data: InputData) -> int:
